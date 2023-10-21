8c561ba1d04f93244f1faddec14c49832559162e
Huda Shakir <huda.f.shakir@gmail.com>
2023-10-21 21:59:22 +0300

    added .net framwork project

---
+++ Binary_Search_Homework_Test/HomeworkTest.cs
@@ -0,0 +1,54 @@
+﻿using Binary_Search_Homework;// DON'T EDIT THIS FILE UNDER ANY CIRCUMSTANCES!!
+using Microsoft.VisualStudio.TestTools.UnitTesting;// DON'T EDIT THIS FILE UNDER ANY CIRCUMSTANCES!!
+// DON'T EDIT THIS FILE UNDER ANY CIRCUMSTANCES!!
+namespace Binary_Search_Homework_Test// DON'T EDIT THIS FILE UNDER ANY CIRCUMSTANCES!!
+{
+    [TestClass]// DON'T EDIT THIS FILE UNDER ANY CIRCUMSTANCES!!
+    public class HomeworkTest // DON'T EDIT THIS FILE UNDER ANY CIRCUMSTANCES!!
+    {
+        [TestMethod]// DON'T EDIT THIS FILE UNDER ANY CIRCUMSTANCES!!
+        public void TestTernarySearch()// DON'T EDIT THIS FILE UNDER ANY CIRCUMSTANCES!!
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+            int key = 10;
+
+            int index = BinarySearchHomework.TernarySearch(arr, key, 0, arr.Length);
+
+            Assert.AreEqual(9, index);
+        }// DON'T EDIT THIS FILE UNDER ANY CIRCUMSTANCES!!
+
+        [TestMethod]// DON'T EDIT THIS FILE UNDER ANY CIRCUMSTANCES!!
+        public void TestGetFirstElement()// DON'T EDIT THIS FILE UNDER ANY CIRCUMSTANCES!!
+        {
+            int[] arr = { 1, 2, 2, 2, 3, 4, 5, 5, 5, 5, 5, 6, 7, 7, 8, 9 };
+            int key = 5;
+
+            // pass true for the first occurrence
+            int first = BinarySearchHomework.BinarySearchForCalculatingRepeated(arr, key, true, 0, arr.Length);
+            Assert.AreEqual(6, first);
+        }// DON'T EDIT THIS FILE UNDER ANY CIRCUMSTANCES!!
+        [TestMethod]// DON'T EDIT THIS FILE UNDER ANY CIRCUMSTANCES!!
+        public void TestGetLastElement()// DON'T EDIT THIS FILE UNDER ANY CIRCUMSTANCES!!
+        {
+            int[] arr = { 1, 2, 2, 2, 3, 4, 5, 5, 5, 5, 5, 6, 7, 7, 8, 9 };
+            int key = 5;
+
+            // pass true for the first occurrence
+            int last = BinarySearchHomework.BinarySearchForCalculatingRepeated(arr, key, false, 0, arr.Length);
+            Assert.AreEqual(10, last);
+        }// DON'T EDIT THIS FILE UNDER ANY CIRCUMSTANCES!!
+
+        [TestMethod]// DON'T EDIT THIS FILE UNDER ANY CIRCUMSTANCES!!
+        public void TestGetRepeatCount()// DON'T EDIT THIS FILE UNDER ANY CIRCUMSTANCES!!
+        {
+            int[] arr = { 1, 2, 2, 2, 3, 4, 5, 5, 5, 5, 5, 6, 7, 7, 8, 9 };
+            int key = 5;
+
+            // pass true for the first occurrence
+            int count = BinarySearchHomework.GetRepeatCount(arr, key);
+            Assert.AreEqual(10, count);
+        }// DON'T EDIT THIS FILE UNDER ANY CIRCUMSTANCES!!
+
+
+    }
+}
---
+++ Binary_Search_Homework_Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Binary_Search_Homework_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Binary_Search_Homework_Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("857e12d5-819a-47a0-97e0-a6077405985b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
