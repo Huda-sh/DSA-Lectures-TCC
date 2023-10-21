03dc3fd3df5946d491c57dbfac82474801031e0e
Huda Shakir <huda.f.shakir@gmail.com>
2023-10-21 21:53:20 +0300

    setup .net framework project

---
+++ Binary_Search_Homework/BinarySearchHomework.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Search_Homework // don't edit this line!!!
+{
+    public class BinarySearchHomework // don't edit this line!!!
+    {
+        public static int TernarySearch(int[] arr, int key, int start, int end) // don't edit this line!!!
+                                                                                // يمكنك تجاهل برمترات البداية والنهاية إذا لا تريد استخدام الطريقة العودية 
+                                                                                // لكن لاتقوم بحذفهم أو التعديل عليهم هنا تحت اي ظرف!!!
+                                                                                // من الممكن تمرير قيمة 0 لتجاهلهم
+        {
+            //TODO: write code for the ternary search algorithm and return the index of the element
+        }
+
+        public static int BinarySearchForCalculatingRepeated
+            (int[] arr, int key, bool is_first, int start, int end) // don't edit this line!!!
+                                                                    // يمكنك تجاهل برمترات البداية والنهاية إذا لا تريد استخدام الطريقة العودية 
+                                                                    // لكن لاتقوم بحذفهم أو التعديل عليهم هنا تحت اي ظرف!!!
+                                                                    // من الممكن تمرير قيمة 0 لتجاهلهم
+        {
+            //TODO: this methods is for getting the first accurence of the key and the last accurance
+        }
+
+        public static int GetRepeatCount(int[] arr, int key) // don't edit this line!!!
+        {
+            //TODO: write code to calculate the repeat count of a spacific element
+            // make sure to use the previous method in this method
+        }
+    }
+}
---
+++ Binary_Search_Homework/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Binary_Search_Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args) // feel free to edit the main method as you like
+        {
+            int[] arr1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+            int key = 10;
+
+            // int idx = BinarySearchHomework.TernarySearch(arr1, key);
+            int idx = BinarySearchHomework.TernarySearch(arr1, key, 0, arr1.Length);
+
+            Console.WriteLine("Found {0} at index {1} using Ternary Search", key, idx);
+
+            Console.WriteLine("=======================");
+
+            int[] arr2 = { 1, 2, 2, 2, 3, 4, 5, 5, 5, 5, 5, 6, 7, 7, 8, 9 };
+            int key2 = 5;
+
+            int repeat_count = BinarySearchHomework.GetRepeatCount(arr2, key2);
+
+            Console.WriteLine("the number 2 is repeated {0} times", repeat_count);
+        }
+    }
+}
