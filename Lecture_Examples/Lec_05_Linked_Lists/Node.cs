8d32494a59c6c4e0dcb722ad8411f9aac2fc68ff
Huda Shakir <huda.f.shakir@gmail.com>
2023-11-10 15:29:09 +0200

    added Linked List lecture example

---
+++ Lecture_Examples/Lec_05_Linked_Lists/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lec_05_Linked_Lists
+{
+    public class Node
+    {
+        // properties
+        public int Data { get; set; }
+        public Node Next { get; set; }
+
+        // constructors
+        public Node() { }
+        public Node(int val)
+        {
+            this.Data = val;
+        }
+    }
+}
