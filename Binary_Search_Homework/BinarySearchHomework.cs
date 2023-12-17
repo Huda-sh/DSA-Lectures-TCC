95747b9d06cc9aa2cf15b095141289185ce922be
Samahkassem2004 <153764444+Samahkassem2004@users.noreply.github.com>
2023-12-17 19:58:16 +0300

    Update BinarySearchHomework.cs

--- Binary_Search_Homework/BinarySearchHomework.cs
+++ Binary_Search_Homework/BinarySearchHomework.cs
@@ -12,10 +12,22 @@ namespace Binary_Search_Homework // don't edit this line!!!
                                                                                 // يمكنك تجاهل برمترات البداية والنهاية إذا لا تريد استخدام الطريقة العودية 
                                                                                 // لكن لاتقوم بحذفهم أو التعديل عليهم هنا تحت اي ظرف!!!
                                                                                 // من الممكن تمرير قيمة 0 لتجاهلهم
-        {
+        {int  []array;
+         array =arr;
+         int n=array.length;
+         int third =n/3;
+         int[] firstpart =new int [third];
+         int[] secondpart =new int [third];
+         int[] thirdpart =new int [n-2*third];
+         for (int i=0;i<third;i++){
+             firstpart[i]=array [i];}
+         for (int i=0;i<third;i++){
+             secondpart[i]=array [i+third];}
+         for (int i=0;i<n-2*third;i++){
+             thirdpart[i]=array [i+2*third];}
             //TODO: write code for the ternary search algorithm and return the index of the element
             return -1;
-        }
+        }return firstpart ;
 
         public static int BinarySearchForCalculatingRepeated
             (int[] arr, int key, bool is_first, int start, int end) // don't edit this line!!!
@@ -25,13 +37,13 @@ namespace Binary_Search_Homework // don't edit this line!!!
         {
             //TODO: this methods is for getting the first accurence of the key and the last accurance
             return -1;
-        }
+        }return secondpart;
 
         public static int GetRepeatCount(int[] arr, int key) // don't edit this line!!!
         {
             //TODO: write code to calculate the repeat count of a spacific element
             // make sure to use the previous method in this method
             return -1;
-        }
+        }return thirdpart;
     }
 }
